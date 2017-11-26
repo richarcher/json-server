5c640dc4043ce9432b19decb78d0b630ec3a8431
Rich Archer <richard.archer@prodigyfinance.com>
2017-11-26 09:11:10 +0200

    Allow `existing_headers` to be reassigned

--- src/server/router/get-existing-access-header.js
+++ src/server/router/get-existing-access-header.js
@@ -1,6 +1,6 @@
 module.exports = function getExistingAccessHeader(res) {
   const headers = ['Location']
-  const existing_headers = res.getHeaders()['access-control-expose-headers']
+  let existing_headers = res.getHeaders()['access-control-expose-headers']
   if (existing_headers !== undefined) {
     if (typeof existing_headers === 'string') {
       existing_headers = existing_headers.split(',')
